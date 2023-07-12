4c28d5aee51d27c1bdaeb296b5dd3c4694bb7516
Victor, Nitish <nvictor@ea.com>
2023-07-12 11:22:16 -0700

    Jump

--- App/Systems/Player.cpp
+++ App/Systems/Player.cpp
@@ -49,6 +49,11 @@ namespace nv
         return 2 * XM_PI * x * (1.f / 360.f);
     }
 
+    const bool AreNearEqual(float a, float b)
+    {
+        return fabs(a - b) < FLT_EPSILON;
+    }
+
     void PlayerController::Init()
     {
         mPlayerEntity = CreateEntity(ID("Mesh/male.fbx"), ID("Bronze"), "Player");
@@ -68,7 +73,9 @@ namespace nv
 
     void PlayerController::Update(float deltaTime, float totalTime)
     {
-
+        using namespace math;
+        const float VELOCITY_DECAY_SPEED = 15.f;
+        const float JUMP_MULTIPLIER = 2.f;
         // TODO:
         // 1. Scene Editor + ImGui +  Scene Serialization/Deserialization
         // 2. Basic Level with Enemies?
@@ -78,25 +85,34 @@ namespace nv
         if (gFrameState == FRAME_RECORD_REWINDING)
             return;
 
+        auto zero = VectorSet(0, 0, 0, 0);
+        auto right = math::VectorSet(1, 0, 0, 0);
+        auto left = math::VectorSet(-1, 0, 0, 0);
+        auto up = math::VectorSet(0, 1, 0, 0);
+        auto epsilon = VectorSet(FLT_EPSILON, FLT_EPSILON, FLT_EPSILON, FLT_EPSILON);
+
         Entity* entity = gEntityManager.GetEntity(mPlayerEntity);
         auto anim = entity->Get<AnimationComponent>();
 
         auto transform = entity->GetTransform();
         const float speed = gpPlayerComponent->mSpeed;
+        auto velocity = Load(gpPlayerComponent->mVelocity);
+        auto maxVelocity = VectorSet(speed, speed * JUMP_MULTIPLIER, speed, speed);
+        auto currentPos = Load(transform.mPosition);
+
+
+        velocity = XMVectorLerp(velocity, zero, deltaTime * VELOCITY_DECAY_SPEED);
+        if(XMVector3NearEqual(velocity, zero, epsilon))
+            velocity = zero;
 
         // TODO: Use Velocity instead of manipulating speed directly. 
         // Apply terms to velocity and then position = position + velocity * time;
-        gpPlayerComponent->mPlayerState = PLAYER_STATE_IDLE;
+        PlayerState state = PLAYER_STATE_IDLE;
+        PlayerState prevState = gpPlayerComponent->mPlayerState;
 
-        if (transform.mPosition.y > 0)
+        if (IsKeyDown(Keys::Space) && AreNearEqual(transform.mPosition.y, 0.f))
         {
-            transform.mPosition.y -= 2.f * deltaTime;
-            gpPlayerComponent->mPlayerState = PLAYER_STATE_JUMP;
-        }
-
-        if (IsKeyDown(Keys::Space))
-        {
-            gpPlayerComponent->mPlayerState = PLAYER_STATE_JUMP;
+            velocity = velocity + up * speed * JUMP_MULTIPLIER;
         }
 
         if (IsKeyDown(Keys::D))
@@ -104,8 +120,7 @@ namespace nv
             auto rotation = XMQuaternionRotationAxis(XMVectorSet(0, 1, 0, 0), DegToRags(-90));
             math::Store(rotation, transform.mRotation);
 
-            transform.mPosition.x += speed * deltaTime;
-            gpPlayerComponent->mPlayerState = PLAYER_STATE_RUNNING;
+            velocity = velocity + right * speed;
         }
 
         if (IsKeyDown(Keys::A))
@@ -113,22 +128,53 @@ namespace nv
             auto rotation = XMQuaternionRotationAxis(XMVectorSet(0, 1, 0, 0), DegToRags(90));
             math::Store(rotation, transform.mRotation);
 
-            transform.mPosition.x -= speed * deltaTime;
-            gpPlayerComponent->mPlayerState = PLAYER_STATE_RUNNING;
+            velocity = velocity + left * speed;
         }
 
+        velocity = XMVectorMin(maxVelocity, velocity);
+        velocity = XMVectorMax(-maxVelocity, velocity);
+        currentPos = currentPos + velocity * deltaTime;
+
+        Store(currentPos, transform.mPosition);
+        Store(velocity, gpPlayerComponent->mVelocity);
+
+        float lDot = XMVectorGetX(Vector3Dot(velocity, left));
+        float rDot = XMVectorGetX(Vector3Dot(velocity, right));
+        float uDot = XMVectorGetX(Vector3Dot(velocity, up));
+
+        if (lDot > 0.1f || rDot > 0.1f)
+        {
+            state = PLAYER_STATE_RUNNING;
+        }
+
+        if (transform.mPosition.y > FLT_EPSILON)
+        {
+            state = PLAYER_STATE_JUMP;
+        }
+
+        gpPlayerComponent->mPlayerState = state;
+
+        anim->mAnimationSpeed = 1.f;
         switch (gpPlayerComponent->mPlayerState)
         {
         case PLAYER_STATE_IDLE:
             anim->mCurrentAnimationIndex = 1;
+            gpPlayerComponent->mVelocity.y = 0.f;
             break;
         case PLAYER_STATE_JUMP:
+            anim->mAnimationSpeed = 0.8f;
             anim->mCurrentAnimationIndex = 2;
+            transform.mPosition.y -= 3.f * deltaTime;
+            transform.mPosition.y = std::max(0.f, transform.mPosition.y);
             break;
         case PLAYER_STATE_RUNNING:
             anim->mCurrentAnimationIndex = 3;
+            gpPlayerComponent->mVelocity.y = 0.f;
             break;
         }
+
+        if(state != prevState)
+            anim->mTotalTime = 0.f;
     }
 
     void PlayerController::Destroy()
